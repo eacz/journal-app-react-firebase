a0e078bdfbdca2d49dbecab70d37ce6b62517b2b
eacz <estebanacanteros@gmail.com>
2021-01-17 20:32:02 -0300

    maintain the session with firebase

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Redirect,
@@ -7,8 +7,19 @@ import {
 } from 'react-router-dom';
 import JournalScreen from '../journal/JournalScreen';
 import AuthRouter from './AuthRouter';
+import { firebase } from '../../firebase/firebaseConfig';
+import { useDispatch } from 'react-redux';
+import { login } from '../../actions/auth';
 
 const AppRouter = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+            }
+        });
+    }, [dispatch]);
     return (
         <Router>
             <Switch>
